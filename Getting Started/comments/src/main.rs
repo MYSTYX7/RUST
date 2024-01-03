893bea5d33324e73a21831440c41929b16bbfe03
Abhinav Singh Chauhan <cool.abhi@outlook.com>
2024-01-03 23:40:39 +0530

    feat: Learnt different types of comments

---
+++ Getting Started/comments/src/main.rs
@@ -0,0 +1,16 @@
+/// Outer Doc Comment
+/// Outer doc comments are written outside the block of code.
+
+fn main() {
+    // Line Comment for a single line
+
+    /*
+        Block comment
+        for more than 1 line
+    */
+
+    //! Inner Doc Comment
+    //! Inner doc comments are written inside the block of code.
+
+    println!("Hello, world!");
+}
